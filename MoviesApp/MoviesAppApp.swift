190145708b269caaa86b9163055831c273dec28b
Prashant Tukadiya <prashant@smilefy.com>
2023-09-01 16:55:27 +0530

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Prashant Tukadiya on 01/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
